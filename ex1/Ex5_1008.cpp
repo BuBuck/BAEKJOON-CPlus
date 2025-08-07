d3e90e6752424f8963d0abaf6917b80abe613fa1
BuBuck <sunjeongin123@gmail.com>
2025-08-08 01:46:49 +0900

    feat: 1008 A/B

---
+++ ex1/Ex5_1008.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    double a, b;
+
+    cin >> a >> b;
+
+    cout.precision(9);
+    cout << fixed;
+    cout << a / b << endl;
+    cout.unsetf(ios::fixed);
+
+    return 0;
+}
